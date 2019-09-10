62a79a335682b4687b607aa1be528b477b3ff1c2
G. Gonzalez <guillermo.gonzalez.gomez@gmail.com>
2019-09-10 11:34:36 +0200

    1.7: unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Value = ({text, value}) => (
-  <p>{text} {value}</p>
-)
+const Statistics = ({good,neutral,bad,all,avg,pos}) => { 
+  return(
+  <div>
+    <h2>statistics</h2>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>all {all}</p>
+    <p>average {avg}</p>
+    <p>positive {pos} %</p>
+  </div>
+  )
+}
 
 const Button = ({clickFunction, text}) => ( 
   <button onClick={clickFunction}>
@@ -17,17 +26,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAvg] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGoodClick = () => {
     setGood(good+1)
+    setAll(all+1)
+    setAvg((good-bad+1)/(all+1))
+    setPositive((good+1)/(all+1)*100)
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral+1)
+    setAll(all+1)
+    setAvg((good-bad)/(all+1))
+    setPositive((good)/(all+1)*100)
   }
 
   const handleBadClick = () => {
     setBad(bad+1)
+    setAll(all+1)
+    setAvg((good-(bad+1))/(all+1))
+    setPositive((good)/(all+1)*100)
   }
 
   return (
@@ -36,10 +57,7 @@ const App = () => {
       <Button clickFunction={handleGoodClick} text='good' />
       <Button clickFunction={handleNeutralClick} text='neutral' />
       <Button clickFunction={handleBadClick} text='bad' />
-      <h2>statistics</h2>
-      <Value text='good' value={good} />
-      <Value text='neutral' value={neutral} />
-      <Value text='bad' value={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} avg={average} pos={positive} />
     </div>
   )
 }
