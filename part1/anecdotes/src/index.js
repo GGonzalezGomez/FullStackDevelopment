989421682bbe340e69beae5c50e830bc145f55e9
G. Gonzalez <guillermo.gonzalez.gomez@gmail.com>
2019-09-18 09:48:59 +0200

    1.14: anecdotes step3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -3,15 +3,31 @@ import ReactDOM from 'react-dom'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState({})
+
+//Array(len).fill(0);
   
   const handleNextButton = () => {
 	  setSelected(Math.floor(Math.random()*props.anecdotes.length))
   }
+  
+  const handleVoteButton = () => {
+	  const copy = { ...votes }
+	  if(votes[selected]=== undefined)
+		  copy[selected] = 1
+	  else
+		  copy[selected] += 1
+	  setVotes(copy)
+  }
  
   return (
     <div>
+      <h2>Anecdote of the day</h2>
       <p>{props.anecdotes[selected]}</p>
+      <Printvotes votes={votes} selected={selected} />
+      <Button text="vote" clickFunction={handleVoteButton} />
       <Button text="next anecdote" clickFunction={handleNextButton} />
+      <Displaymaxvotes votes={votes} anecdotes={props.anecdotes} />
     </div>
   )
 }
@@ -30,6 +46,38 @@ const Button = ({clickFunction, text}) => (
 	  {text}
 	</button>
 )
+
+const Printvotes = ({votes,selected}) => {
+	var v
+	if(votes[selected] === undefined)
+		v=0
+	else
+		v=votes[selected]
+	return(
+		<p>has {v} votes</p>
+	)
+}
+
+const Displaymaxvotes = ({votes,anecdotes}) => {
+	var maxValue=0
+	var maxAnecdote
+
+	for (const an in votes) {
+		if(votes[an]>maxValue && votes[an]!== undefined){
+			maxAnecdote=anecdotes[an]
+			maxValue=votes[an]
+		}
+	}
+	if(maxValue>0){
+		return (
+			<div>
+				<h2>Anecdote with most votes</h2>
+				<p>{maxAnecdote}</p>
+			</div>
+		)
+	}
+	return null
+}
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
