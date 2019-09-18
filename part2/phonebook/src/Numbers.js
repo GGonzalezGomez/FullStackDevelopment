94adb1296cf6c2d2eec9a970d3e483b63500ec5f
G. Gonzalez <guillermo.gonzalez.gomez@gmail.com>
2019-09-18 16:51:07 +0200

    2.6: The Phonebook step1

---
+++ part2/phonebook/src/Numbers.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Person = ({p}) => {
+	return(
+		<p>{p.name}</p>
+	)
+}
+
+const Printpersons = ({persons}) => persons.map(p => <Person p={p} key={p.name} />)
+
+const Numbers = ({persons}) => {
+
+  return (
+    <div>
+      <h2>Numbers</h2>
+      <Printpersons persons={persons} />
+    </div>
+  )
+}
+
+export default Numbers
+
