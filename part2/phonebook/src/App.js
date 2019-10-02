299a04a12c40cf55f4ea3ac4d92fa7342f36037c
Guillermo Gonzalez <guillermo.gonzalez.gomez@gmail.com>
2019-10-02 20:58:39 +0200

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Numbers from './Numbers'
 import Filter from './Filter'
 import Person from './Person'
 
 const App = (props) => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'},
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ newFilter, setFilter ] = useState('')
+
+  const effectHook = () => {
+	  console.log("Running Effect Hook")
+	  axios
+	    .get('http://localhost:3001/persons').then(response => {
+		    console.log('Response received')
+		    setPersons(response.data)
+	    })
+  }
+
+  useEffect(effectHook,[])
 
   const addNumber = (event) => {
 	  event.preventDefault()
