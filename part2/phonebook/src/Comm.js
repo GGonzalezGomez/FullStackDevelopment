52016fac29cc0131e05c6c1ae0033b6e45c324ed
G. Gonzalez <guillermo.gonzalez.gomez@gmail.com>
2019-12-31 13:53:36 +0100

    2.20: Phonebook step11 and step12

--- part2/phonebook/src/Comm.js
+++ part2/phonebook/src/Comm.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://192.168.1.32:3001/persons'
+const baseURL = 'http://192.168.56.99:3001/persons'
 
 const getAll = () => {
 	const request = axios.get(baseURL)
