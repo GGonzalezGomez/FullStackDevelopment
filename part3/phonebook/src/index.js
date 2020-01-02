3634df1b6ff570203ece52a88a56d2f2a4c3a43a
G. Gonzalez <guillermo.gonzalez.gomez@gmail.com>
2020-01-02 11:23:37 +0100

    3.4: Phonebook backend step4

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -51,6 +51,13 @@ app.get('/api/persons/:id', (request, response) => {
 
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== parseInt(id) )
+  response.status(204).end()
+})
+
+
 app.get('/info', (request, response) => {
   response.send('<div><p>Phonebook has info for ' + persons.length + ' people</p><p>' + Date() + '</p></div>')
 })
