b6069d47527bdcc4e080a49e64b11156ee8e09d9
Guillermo Gonzalez <guillermo.gonzalez@alliance-healthcare.net>
2020-01-07 16:44:30 +0100

    4.5: helper functions and unit tests, step3

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,10 +6,27 @@ const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
     return sum + item.likes
   }
-  return blogs.reduce(reducer, 0)
+  if(blogs.length!=0)
+    return blogs.reduce(reducer, 0)
+  else
+    return 0
+}
+
+const favoriteBlog = (blogs) => {
+  const reducer = (fav, item) => {
+    if(fav.likes<item.likes)
+      return {'title': item.title, 'author': item.author, 'likes': item.likes}
+    else
+      return {'title': fav.title, 'author': fav.author, 'likes': fav.likes}
+  }
+  if(blogs.length!=0)
+    return blogs.reduce(reducer,blogs[0])
+  else
+    return {}
 }
   
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
