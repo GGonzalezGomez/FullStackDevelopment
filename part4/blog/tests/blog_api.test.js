fd1a1f60f19602a241fe168eb10dad9799635993
Guillermo Gonzalez <guillermo.gonzalez@alliance-healthcare.net>
2020-01-08 13:39:53 +0100

    4.10: Blog list tests, step3

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
@@ -18,6 +19,25 @@ test('blogs count is correct', async () => {
 test('id field exists', async () => {
   const response = await api.get('/api/blogs')
   expect(response.body[0].id).toBeDefined()
+})
+
+test('new posts are correct', async () => {
+  // Get current post count
+  const prev = await api.get('/api/blogs')
+  // Insert a new one
+  const newBlog = {
+    'title': 'This is an auto posted entry',
+    'author': 'Autotester',
+    'url': 'http://example.com/autotester',
+    'likes': 11
+  }
+  const response = await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type', /application\/json/)
+  // Get the current count
+  const after = await api.get('/api/blogs')
+  // Delete the new post
+  await api.delete('/api/blogs/'+response.body.id)
+
+  expect(after.body.length).toBe(prev.body.length+1)
 })
 
 afterAll(() => {
