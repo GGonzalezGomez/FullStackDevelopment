64eda94c649da40e442b182a432721fd1e733a87
Guillermo Gonzalez <guillermo.gonzalez@alliance-healthcare.net>
2020-01-13 15:17:45 +0100

    5.14: Blog list tests, step2

--- part5/bloglist/src/components/SimpleBlog.test.js
+++ part5/bloglist/src/components/SimpleBlog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
 test('renders content', () => {
@@ -11,25 +11,31 @@ test('renders content', () => {
     likes: 3
   }
 
-  const handleClick = (event) => {
-      console.log(event.target)
-  }
-
+  const mockHandler = jest.fn()
+/*
   const component = render(
-    <SimpleBlog blog={blog} onClick={handleClick} />
+    <SimpleBlog blog={blog} onClick={mockHandler} />
+  )
+*/
+  const { container, getByText } = render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />
   )
 
-  component.debug()
+  const button = getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
 
-  expect(component.container).toHaveTextContent(
+  expect(mockHandler.mock.calls.length).toBe(2)
+
+  expect(container).toHaveTextContent(
     'This is a blog test'
   )
 
-  expect(component.container).toHaveTextContent(
+  expect(container).toHaveTextContent(
     'Guillermo Gonzalez'
   )
 
-  expect(component.container).toHaveTextContent(
+  expect(container).toHaveTextContent(
     'blog has 3 likes'
   )
 
