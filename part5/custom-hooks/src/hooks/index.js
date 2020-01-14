9282a51c7e9ca679e99d984d045d1c035f0046e3
Guillermo Gonzalez <guillermo.gonzalez@alliance-healthcare.net>
2020-01-14 17:19:13 +0100

    WIP 5.21

---
+++ part5/custom-hooks/src/hooks/index.js
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+export const useResource = (baseUrl) => {
+    const [resources, setResources] = useState([])
+    let token = null
+  
+    const create = async newObject => {
+        const config = {
+            headers: { Authorization: token },
+        }
+
+        const response = await axios.post(baseUrl, newObject, config)
+        return response.data
+    }
+
+    const setToken = (newToken) => {
+        token = `bearer ${newToken}`
+    }
+
+    const getAll = () => {
+        const request = axios.get(baseUrl)
+        return request.then(response => response.data)
+    }
+
+    const update = (id, newObject) => {
+        const request = axios.put(`${ baseUrl } /${id}`, newObject)
+        return request.then(response => response.data)
+    }
+
+    const updateResources = (newResources) => {
+        setResources(newResources)
+    }
+
+    
+    const service = {
+        create, setToken, getAll, update, updateResources
+    }
+
+//    const service = {
+        /*
+        setToken: (newToken) => {
+            token = `bearer ${newToken}`
+        },
+
+        getAll : () => {
+            const request = axios.get(baseUrl)
+            return request.then(response => response.data)
+        },
+  
+        create : async newObject => {
+            const config = {
+                headers: { Authorization: token },
+            }
+  
+            const response = await axios.post(baseUrl, newObject, config)
+            return response.data
+        },
+  
+        update : (id, newObject) => {
+            const request = axios.put(`${ baseUrl } /${id}`, newObject)
+            return request.then(response => response.data)
+        },
+
+        updateResources: (newResources) => {
+            setResources(newResources)
+        }
+        */
+//       service: 'Hola'
+//    }
+    
+  
+    return [resources, service ]
+  }
